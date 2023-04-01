18dd1305389512a7204d6ab8c26226167e61260d
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-04-01 12:58:38 +0200

    gjenskaopt

---
+++ Quiz 1/Class1.cs
@@ -0,0 +1,40 @@
+﻿namespace Quiz_1
+{
+    public class Question
+    {
+        public string QuestionText { get; private set; }
+        public string[] Options { get; private set; }
+        public char RightAnswer { get; private set; }
+        public Question(string questionText, string[] options, char rightAnswer)
+        {
+            QuestionText = questionText;
+            Options = options;
+            RightAnswer = rightAnswer;
+        }
+        public int RunQuestion()
+        {
+            Console.WriteLine(QuestionText);
+            for (int i = 0; i < Options.Length; i++)
+            {
+                var letter = (Char)('A' + i);
+                string? question = Options[i];
+                Console.WriteLine(letter + ": " + question);
+            }
+            Console.Write("Skriv inn svaret ditt: ");
+            var answer = Console.ReadLine();
+            if (answer[0] == RightAnswer)
+            {
+                Console.WriteLine("Svaret er riktig.");
+                Console.WriteLine();
+                return 1;
+            }
+
+            Console.WriteLine("Svaret er feil!");
+            Console.WriteLine();
+            return 0;
+
+        }
+
+    }
+}
+
---
+++ Quiz 1/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Quiz_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] optionsArray = { "En karakter fra en visuell skrekkroman", "En Arkitekt", "En guvernør i Elysium" };
+            var Question1 = new Question("Hvem er Samuel Ayers", optionsArray, 'a');
+            string[] optionsArray2 = { "Minearbeider", "Butikkmedarbeider", "Utforsker", "Sheriff", "Hore" };
+            var Question2 = new Question("Hva er Sams opprinnelige yrke?", optionsArray2, 'e');
+            string[] optionsArray3 = { "Han falt ned fra en klippe", "Han druknet i innsjøen", "Han ble drept av en tulpa" };
+            var Question3 = new Question("Hvordan døde Sydney?", optionsArray3, 'b');
+            string[] optionsArray4 = { "Drukna av seg selv", "Chase", "Flynn", "Jenna", "Tobias", "Carl", "Leo" };
+            var Question4 = new Question("Hvem drukna Sydney", optionsArray4, 'b');
+            int points = 0;
+            points += Question1.RunQuestion();
+            points += Question2.RunQuestion();
+            points += Question3.RunQuestion();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            points += Question4.RunQuestion();
+            Console.WriteLine(points + " Poeng");
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+
+
+
+
+
+            //var points = 0;
+            //Console.WriteLine("Hva er 2+2?");
+            //Console.WriteLine(" A: 2");
+            //Console.WriteLine(" B: 3");
+            //Console.WriteLine(" C: 4");
+            //Console.Write("Skriv inn a, b eller c: ");
+            //var answer1 = Console.ReadLine();
+            //if (answer1 == "c")
+            //{
+            //    points++;
+            //    Console.WriteLine("Riktig!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Feil");
+            //}
+            //Console.WriteLine("Hvilken by er norsk?");
+            //Console.WriteLine(" A: Oslo");
+            //Console.WriteLine(" B: Ohio");
+            //Console.WriteLine(" C: Omaha");
+            //Console.Write("Skriv inn a, b eller c: ");
+            //var answer2 = Console.ReadLine();
+            //if (answer2 == "a")
+            //{
+            //    points++;
+            //    Console.WriteLine("Riktig!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Feil");
+            //}
+            //Console.WriteLine("Hvilken farge er ikke i den norske flagget?");
+            //Console.WriteLine(" A: Rød");
+            //Console.WriteLine(" B: Grønn");
+            //Console.WriteLine(" C: Blå");
+            //Console.Write("Skriv inn a, b eller c: ");
+            //var answer3 = Console.ReadLine();
+            //if (answer3 == "b")
+            //{
+            //    points++;
+            //    Console.WriteLine("Riktig!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Feil");
+            //}
+
+            //Console.WriteLine($"Du fikk {points} riktige.");
+        }
+
+    }
+}
